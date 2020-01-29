8d4bc7fc71ec08a7853596e0d0a98a9c4c6fb547
Akratash <patrickbuehler@hotmail.de>
2020-01-29 14:43:19 +0100

    fix some issues

--- Notentool/src/ch/akratash/notentool/App.java
+++ Notentool/src/ch/akratash/notentool/App.java
@@ -7,11 +7,11 @@ import ch.akratash.notentool.model.Noten;
 public class App {
 	
 	private Scanner scanner = new Scanner(System.in);
-	private Noten note;
+	private NotenRechner nRechner = new NotenRechner();
+	private Noten note = new Noten();
 	private double[] ergebnisse = new double[10];
 	private int totGewichtung = 0;
 	private double notenSchnitt = 0;
-
 	private int index = -1;
 
 	public static void main(String[] args) {
@@ -24,16 +24,16 @@ public class App {
 		System.out.println("[Noten-Tool-Start]");
 		gibEin();
 		gibAus();
+		System.out.println("[Noten-Tool-Ende]");
 		
 	}
 	
 	private void gibEin() {
 		int zähler = 0;
-		NotenRechner nRechner = new NotenRechner();
-		note = new Noten();
 		System.out.print("[Geben Sie die anzahl Noten ein] > ");
 		int notenAnzahl = scanner.nextInt();
 		while(zähler<notenAnzahl) {
+			
 			System.out.print("\n[Noteneingabe (1-6)] > ");
 			double mark = scanner.nextDouble();
 			if(mark >= NotenRechner.MIN_N_GW && mark <= NotenRechner.MAX_N) {
@@ -42,8 +42,6 @@ public class App {
 				System.out.println("[Falsche Eingabe - Programm Ende]");
 				System.exit(0);
 			}
-			
-			
 			
 			System.out.print("\n[Gewichtung (1-4)] > ");
 			int gew = scanner.nextInt();
@@ -63,12 +61,11 @@ public class App {
 		}
 	}
 	
-	
 	private void gibAus() {
 		for (int i = 0; i < ergebnisse.length; i++) {
 			notenSchnitt += ergebnisse[i];
 		}
 		notenSchnitt = notenSchnitt/totGewichtung;
-		System.out.print("\n[Der Notenschnitt beträgt] > ["+notenSchnitt+"]\n[Noten-Tool-Ende]");
+		System.out.print("\n[Der Notenschnitt beträgt] > ["+notenSchnitt+"]");
 	}
 }
